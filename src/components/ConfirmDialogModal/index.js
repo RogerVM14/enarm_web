fa7936ded581ac15e8129f241205a342c4ad6a12
RogerVM14 <30475562+RogerVM14@users.noreply.github.com>
2024-11-25 15:55:47 -0600

    Fixing UI issues with text Cancelar

--- src/components/ConfirmDialogModal/index.js
+++ src/components/ConfirmDialogModal/index.js
@@ -73,7 +73,7 @@ const ConfirmDialogModal = ({
               <button
                 onClick={onCancel}
                 type="button"
-                className="w-full sm:w-auto py-2 px-4 sm:py-2.5 sm:px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                className="w-full sm:w-auto py-2 px-4 sm:py-2.5 sm:px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600"
               >
                 {cancelText}
               </button>
